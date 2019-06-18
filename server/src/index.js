dedf32af7d63f81a0bc198787289b5a54eb1bb6b
Philip Simoneau <phillip.simoneau@advent.com>
2019-06-18 17:29:49 -0400

    separate servers

--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// declare shared dependencies as externals / global
-window.React = React;
-
-ReactDOM.render(<App />, document.querySelector("#root"))
--- server/src/plugins/Comp2/Root.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Comp2() {
-    return (
-        <div>Comp2</div>
-    );
-}
